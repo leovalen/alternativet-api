572a538b10f767f1b571e709cbbbcb423e9dd478
Leo Valen <leo.valen@fagforbundet.no>
2016-03-26 16:23:08 +0100

    edit: org structure models

--- app/Organisation.php
+++ app/Organisation.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Organisation extends Model
 {
+    protected $fillable = ['name', 'type', 'orgnr'];
+
     public function appointments()
     {
         $this->belongsToMany('App\Appointment');
