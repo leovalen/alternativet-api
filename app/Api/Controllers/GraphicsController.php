9b33ec1690f2a9f3967253b82417b18e827a1bd9
Leo Valen <leo.valen@fagforbundet.no>
2016-03-28 22:59:57 +0200

    edit: members map graphic

--- app/Api/Controllers/GraphicsController.php
+++ app/Api/Controllers/GraphicsController.php
@@ -4,6 +4,7 @@ namespace Api\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
+use Illuminate\Support\Facades\DB;
 
 class GraphicsController extends BaseController
 {
@@ -14,16 +15,36 @@ class GraphicsController extends BaseController
     {
         $svg = simplexml_load_file(base_path() . '/resources/svg/Norway_municipalities_2012_blank.svg');
 
-        // dd($svg);
+        // Determine which municipalities there are members in
+        $result = DB::select( DB::raw("
+          select postal_areas.municipality_code, count(*) as count
+          from users
+          left join postal_areas
+          on (users.postal_code = postal_areas.postal_code)
+          group by municipality_code"));
 
-        
+        $municipalities = [];
+        foreach ($result as $row)
+        {
+            if ( $row->municipality_code )
+            {
+                $municipalities[] = $row->municipality_code;
+            }
+        }
 
         foreach ( $svg->g as $county )
         {
             foreach ($county->path as $path)
             {
-                $path['style'] = 'fill:#555;fill-opacity:1;stroke:none';
-                //print_r($path['id']);
+                if ( in_array( $path['id'], $municipalities ) )
+                {
+                    // There are members in this municipality
+                    $path['style'] = 'fill:#0f0;fill-opacity:1;stroke:none';
+                }
+                else
+                {
+                    $path['style'] = 'fill:#777;fill-opacity:1;stroke:none';
+                }
             }
         }
         return response($svg->asXML())
