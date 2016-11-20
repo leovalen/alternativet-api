b1eeadd066a0b22f19dc8452174f2c8d048f4b30
Leo Valen <leo.valen@fagforbundet.no>
2016-11-20 13:42:52 +0100

    added condorcet

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\SetPasswords::class
+        Commands\SetPasswords::class,
+        Commands\CountElection::class
     ];
 
     /**
